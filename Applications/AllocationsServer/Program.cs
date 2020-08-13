2b28668822787d0cb53470ef393af27198459a50
binilv <binil_v@dell.com>
2020-08-13 15:12:10 +0530

    SD1.1

---
+++ Applications/AllocationsServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Logging;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Common.Hosting;
+
+namespace AllocationsServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseCloudHosting()
+                .AddCloudFoundry()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging((hostingContext, loggingBuilder) =>
+                {
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    loggingBuilder.AddDynamicConsole();
+                });
+    }
+}
---
+++ Applications/RegistrationServer/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Accounts;
+using Accounts.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Projects.Data;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Users.Data;
+using Steeltoe.Discovery.Client;
+
+namespace RegistrationServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCloudFoundryActuators(Configuration);
+
+            services.AddControllers();
+            services.AddDiscoveryClient(Configuration);
+
+            services.AddScoped<IAccountDataGateway, AccountDataGateway>();
+            services.AddDbContext<AccountContext>(options => options.UseMySql(Configuration));
+
+            services.AddScoped<IUserDataGateway, UserDataGateway>();
+            services.AddDbContext<UserContext>(options => options.UseMySql(Configuration));
+
+            services.AddScoped<IProjectDataGateway, ProjectDataGateway>();
+            services.AddDbContext<ProjectContext>(options => options.UseMySql(Configuration));
+            
+            services.AddScoped<IRegistrationService, RegistrationService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDiscoveryClient();
+            app.UseCloudFoundryActuators();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
